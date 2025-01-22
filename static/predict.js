5f7dbcc79620ab43a07d869532e8126deb9f1b93
cyrilclovis <cyrilclovis@gmail.com>
2025-01-22 17:54:17 +0100

    Intégration de l'IA triomph

--- static/predict.js
+++ static/predict.js
@@ -19,7 +19,7 @@ $( document ).ready(async function () {
 	modelLoaded = false;
 	$('.progress-bar').show();
     console.log( "Loading model..." );
-    model = await tf.loadGraphModel('model/model.json');
+    model = await tf.loadGraphModel('model/IA-v2_tfjs/model.json');
     console.log( "Model loaded." );
 	$('.progress-bar').hide();
 	modelLoaded = true;
@@ -30,28 +30,49 @@ $("#predict-button").click(async function () {
 	if (!imageLoaded) { alert("Please select an image first"); return; }
 	
 	let image = $('#selected-image').get(0);
+
+	const preprocessImageToTensor = (image) => {
+		try {
+		  // Convert the image to a tensor
+		  const imgTensor = tf.browser.fromPixels(image);
+	
+		  // Resize the image to the expected size
+		  const resizedTensor = tf.image.resizeBilinear(imgTensor, [224, 224]).toFloat();
+	  
+		  // Normalize the image
+		  const mean = [0.485, 0.456, 0.406];
+		  const std = [0.229, 0.224, 0.225];
+		  const normalizedTensor = resizedTensor.div(tf.scalar(255.0))
+			.sub(tf.tensor(mean))
+			.div(tf.tensor(std));
+	  
+		  // Add a batch dimension
+		  const batchedTensor = normalizedTensor.expandDims(0);
+	  
+		  return batchedTensor;
+		} catch (error) {
+		  console.error("Error transforming image to tensor: ", error);
+		  throw new Error("Error during image preprocessing");
+		}
+	};
 	
 	// Pre-process the image
 	console.log( "Loading image..." );
-	let tensor = tf.browser.fromPixels(image, 3)
-		.resizeNearestNeighbor([224, 224]) // change the image size
-		.expandDims()
-		.toFloat()
-		.reverse(-1); // RGB -> BGR
-	let predictions = await model.predict(tensor).data();
-	console.log(predictions);
-	let top5 = Array.from(predictions)
-		.map(function (p, i) { // this is Array.map
-			return {
-				probability: p,
-				className: TARGET_CLASSES[i] // we are selecting the value from the obj
-			};
+	let tensor = preprocessImageToTensor(image);
+	let predictions = await model.predict(tensor).array();
+	console.log(predictions[0]);
+	let top8  = predictions[0].map((score, index) => {
+		return {
+		  score: score,
+		  className: TARGET_CLASSES[index] // Sélection de la valeur depuis l'objet
+		};
 		}).sort(function (a, b) {
-			return b.probability - a.probability;
-		}).slice(0, 2);
+			return b.score - a.score;
+		}).slice(0, 7);
 
 	$("#prediction-list").empty();
-	top5.forEach(function (p) {
-		$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+	top8.forEach(function (p) {
+		console.log(p.className + ": "  + p.score);
+		$("#prediction-list").append(`<li>${p.className}: ${p.score}</li>`);
 		});
 });
